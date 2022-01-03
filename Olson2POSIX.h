54bf93815962409d69c5b08ade57961b07241024
GuruSR <17853390+GuruSR@users.noreply.github.com>
2022-01-02 20:53:56 -0500

    Version 1.0

---
+++ Olson2POSIX.h
@@ -0,0 +1,25 @@
+#ifndef WATCHY_OLSON2POSIX_H
+#define WATCHY_OLSON2POSIX_H
+
+#include <Arduino.h>
+#include <Arduino_JSON.h>
+#include <WiFiClient.h>
+#include <HTTPClient.h>
+#include <WiFi.h>
+#include <time.h>
+
+class Olson2POSIX{
+    public:
+        String getPOSIX(String inOlson);
+        bool setCurrentTimeZone();
+        bool setOlsonTimeZone(String inOlson);
+        String getCurrentPOSIX();
+        String getCurrentOlson();
+        bool setCurrentPOSIX(String NewPOSIX);
+        bool beginOlsonFromWeb();
+        void endOlsonFromWeb();
+        bool gotOlsonFromWeb();
+        void init();
+        const String TZMISSING = "--MISSING--";
+};
+#endif
